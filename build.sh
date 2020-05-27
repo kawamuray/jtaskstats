e6f7abbd5d47bdb03d7c59e2726077ed16fcd61c
Yuto Kawamura <kawamuray.dadada@gmail.com>
2020-05-27 23:03:24 +0900

    Code cleanup build.sh

--- build.sh
+++ build.sh
@@ -5,8 +5,5 @@ root_dir="$(dirname $0)"
 
 $root_dir/jthreadinfo/gradlew --build-file $root_dir/jthreadinfo/build.gradle build
 
-cd $root_dir
-
-# export RUSTFLAGS="$RUSTFLAGS -C linker=musl-gcc"
 cargo build --manifest-path=$root_dir/Cargo.toml
 cargo test --manifest-path=$root_dir/Cargo.toml
